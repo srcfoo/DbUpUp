6e7185f04c73ab480a003f96fbc8f09bc064be68
Eric Peterson <eric@ericpeterson.mn>
2014-10-28 17:25:03 -0500

    Adding missing comments to databaseversion class

--- src/DbUp/Engine/DatabaseVersion.cs
+++ src/DbUp/Engine/DatabaseVersion.cs
@@ -6,11 +6,18 @@ using System.Text;
 
 namespace DbUp.Engine
 {
+    /// <summary>
+    /// Represents the current version of the database
+    /// </summary>
     public class DatabaseVersion
     {
         private string version;
         private string dbConn;
 
+        /// <summary>
+        /// Setup a connection to the database and get the current version
+        /// </summary>
+        /// <param name="dbConn">The database connection string</param>
         public DatabaseVersion(string dbConn) {
             this.dbConn = dbConn;
             this.getVersion();
@@ -29,6 +36,10 @@ namespace DbUp.Engine
             }
         }
 
+        /// <summary>
+        /// Get the current database version from the database
+        /// </summary>
+        /// <returns>Most recent version hash from the database</returns>
         private string getVersion() {
             using (SqlConnection conn = new SqlConnection(this.dbConn))
             {
@@ -47,6 +58,11 @@ namespace DbUp.Engine
             return this.version;
         }
 
+        /// <summary>
+        /// Set the most recent database version and store it in the database
+        /// </summary>
+        /// <param name="hash">The version the database is now at</param>
+        /// <returns>True on success and false on failure</returns>
         private bool setVersion(string hash)
         {
             using(SqlConnection conn = new SqlConnection(this.dbConn))
