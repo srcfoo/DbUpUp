0b890de28d485e07abd272227d0c30a31d47680c
arinet_fredrickson <jfredrickson@mnl-fredrick-w7.fbsm.com>
2015-02-04 11:08:45 -0600

    Updated the printAll logic so it will print to the logger and not the console. Updated the console logger's error method to write to standard error so that octopus will report an error Updated the git executeCommand method so that it does not write to standard error since git checkout [branch] will return "Already on [branch]" messages to standard error/warning which causes octopus to report an error.

--- src/DbUp.Console/Program.cs
+++ src/DbUp.Console/Program.cs
@@ -1,31 +1,28 @@
-﻿using System;
-using System.IO;
-using System.Data.SqlClient;
+﻿using DbUp.Engine;
 using NDesk.Options;
-using DbUp.Engine;
+using System;
+using System.Data.SqlClient;
+using System.IO;
 
-namespace DbUp.Console
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var server = "";
-            var database = "";
-            var directory = "";
-            var username = "";
-            var password = "";
-            var connectionString = "";
-            var workingDir = "";
-            var dryrun = false;
-            var printAll = false;
-            var headVersion = "";
-            var promptUser = false;
-			var branch  = "";
+namespace DbUp.Console {
+	class Program {
+		static void Main(string[] args) {
+			var server = "";
+			var database = "";
+			var directory = "";
+			var username = "";
+			var password = "";
+			var connectionString = "";
+			var workingDir = "";
+			var dryrun = false;
+			var printAll = false;
+			var headVersion = "";
+			var promptUser = false;
+			var branch = "";
 
-            bool show_help = false;
+			bool show_help = false;
 
-            var optionSet = new OptionSet() {
+			var optionSet = new OptionSet() {
                 { "s|server=", "the SQL Server host", s => server = s },
                 { "db|database=", "database to upgrade", d => database = d},
                 { "d|directory=", "directory containing SQL Update files", dir => directory = dir },
@@ -40,95 +37,81 @@ namespace DbUp.Console
                 { "b|branch=", "Git branch to checkout and pull", b => branch = b }
             };
 
-            optionSet.Parse(args);
+			optionSet.Parse(args);
 
-            if (args.Length == 0)
-                show_help = true;
+			if (args.Length == 0) {
+				show_help = true;
+			}
 
-            if (show_help)
-            {
-                optionSet.WriteOptionDescriptions(System.Console.Out);
-                return;
+			if (show_help) {
+				optionSet.WriteOptionDescriptions(System.Console.Out);
+				return;
 
-            }
+			}
 
-            Directory.SetCurrentDirectory(workingDir);
+			Directory.SetCurrentDirectory(workingDir);
 
-            if (String.IsNullOrEmpty(connectionString))
-            {
-                connectionString = BuildConnectionString(server, database, username, password);
-            }
+			if (String.IsNullOrEmpty(connectionString)) {
+				connectionString = BuildConnectionString(server, database, username, password);
+			}
 
-            // Get the version hash of the database and repo@HEAD
-            branch = !String.IsNullOrEmpty(branch) ? branch : null;
-            DatabaseVersion databaseVersion = new DatabaseVersion(connectionString);
-            Git aGit = new Git(workingDir, branch);
-            aGit.UpdateLocalRepo();
-            headVersion = aGit.HeadVersion();
+			// Get the version hash of the database and repo@HEAD
+			branch = !String.IsNullOrEmpty(branch) ? branch : null;
+			DatabaseVersion databaseVersion = new DatabaseVersion(connectionString);
+			Git aGit = new Git(workingDir, branch);
+			aGit.UpdateLocalRepo();
+			headVersion = aGit.HeadVersion();
 
-            LogRunDetails(databaseVersion.Version, headVersion, connectionString, workingDir);
+			LogRunDetails(databaseVersion.Version, headVersion, connectionString, workingDir);
 
-            var dbup = DeployChanges.To
-                .SqlDatabase(connectionString)
-                .LogToConsole()
-                .WithScriptsFromFileSystem(directory)
-                .Build(branch);
+			var dbup = DeployChanges.To
+				.SqlDatabase(connectionString)
+				.LogToConsole()
+				.WithScriptsFromFileSystem(directory)
+				.Build(branch);
 
-            if (dryrun)
-            {
-                dbup.DryRun(databaseVersion.Version, headVersion, workingDir, printAll, connectionString);
-            }
-            else
-            {
-                if (dbup.IsUpgradeRequired(databaseVersion.Version, workingDir))
-                {
-                    if (dbup.PerformUpgrade(databaseVersion.Version, headVersion, workingDir, connectionString).Successful)
-                    {
-                        // Set the new database version
-                        databaseVersion.Version = headVersion;
-                        System.Console.WriteLine("Database updated to " + headVersion);
-                    }
-                }
-                else
-                {
-                    System.Console.WriteLine("\r\n\r\nDatabase already at newest version. Upgrade is not required.");
-                }
-            }
+			if (dryrun) {
+				dbup.DryRun(databaseVersion.Version, headVersion, workingDir, printAll, connectionString);
+			} else {
+				if (dbup.IsUpgradeRequired(databaseVersion.Version, workingDir)) {
+					if (dbup.PerformUpgrade(databaseVersion.Version, headVersion, workingDir, connectionString).Successful) {
+						// Set the new database version
+						databaseVersion.Version = headVersion;
+						System.Console.WriteLine("Database updated to {0}", headVersion);
+					}
+				} else {
+					System.Console.WriteLine("\r\n\r\nDatabase already at newest version. Upgrade is not required.");
+				}
+			}
 
-            if (promptUser)
-            {
-                System.Console.WriteLine("\r\n\r\nPress any key to continue.");
-                System.Console.ReadKey();
-            }
-        }
+			if (promptUser) {
+				System.Console.WriteLine("\r\n\r\nPress any key to continue.");
+				System.Console.ReadKey();
+			}
+		}
 
-        private static string BuildConnectionString(string server, string database, string username, string password)
-        {
-            var conn = new SqlConnectionStringBuilder();
-            conn.DataSource = server;
-            conn.InitialCatalog = database;
-            if (!String.IsNullOrEmpty(username))
-            {
-                conn.UserID = username;
-                conn.Password = password;
-                conn.IntegratedSecurity = false;
-            }
-            else
-            {
-                conn.IntegratedSecurity = true;
-            }
+		private static string BuildConnectionString(string server, string database, string username, string password) {
+			var conn = new SqlConnectionStringBuilder();
+			conn.DataSource = server;
+			conn.InitialCatalog = database;
+			if (!String.IsNullOrEmpty(username)) {
+				conn.UserID = username;
+				conn.Password = password;
+				conn.IntegratedSecurity = false;
+			} else {
+				conn.IntegratedSecurity = true;
+			}
 
-            return conn.ToString();
-        }
+			return conn.ToString();
+		}
 
-        private static void LogRunDetails(string databaseVersion, string headVersion, string connectionString, string workingDir)
-        {
-            System.Console.WriteLine("--------------------------------------------------------------------------------");
-            System.Console.WriteLine("Connection: " + connectionString);
-            System.Console.WriteLine("Working Directory: " + workingDir);
-            System.Console.WriteLine("DB Version: " + databaseVersion);
-            System.Console.WriteLine("HEAD Version: " + headVersion);
-            System.Console.WriteLine("--------------------------------------------------------------------------------");
-        }
-    }
+		private static void LogRunDetails(string databaseVersion, string headVersion, string connectionString, string workingDir) {
+			System.Console.WriteLine("--------------------------------------------------------------------------------");
+			System.Console.WriteLine("Connection: {0}", connectionString);
+			System.Console.WriteLine("Working Directory: {0}", workingDir);
+			System.Console.WriteLine("DB Version: {0}", databaseVersion);
+			System.Console.WriteLine("HEAD Version: {0}", headVersion);
+			System.Console.WriteLine("--------------------------------------------------------------------------------");
+		}
+	}
 }
--- src/DbUp/Engine/Git.cs
+++ src/DbUp/Engine/Git.cs
@@ -2,124 +2,114 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.IO;
 using System.Diagnostics;
 
-namespace DbUp.Engine
-{
-    /// <summary>
-    /// Git commands to find migrations that need to be executed since the last release
-    /// </summary>
-    public class Git
-    {
-        // Location of the git repository on the local machine
-        private string workingDir = "";
-        // git.exe should be on the user's path
-        private string gitExe = ""; //"git.exe";
-        // The branch you want to use
-        private string branch = ""; //"master";
-        // Name of the remote to use
-        private string remote = ""; //"origin";
+namespace DbUp.Engine {
+	/// <summary>
+	/// Git commands to find migrations that need to be executed since the last release
+	/// </summary>
+	public class Git {
+		// Location of the git repository on the local machine
+		private string workingDir = "";
+		// git.exe should be on the user's path
+		private string gitExe = ""; //"git.exe";
+		// The branch you want to use
+		private string branch = ""; //"master";
+		// Name of the remote to use
+		private string remote = ""; //"origin";
 
-        public Git(string workingDir, string branch)
-        {
-            this.workingDir = workingDir;
+		public Git(string workingDir, string branch) {
+			this.workingDir = workingDir;
 
-            Hashtable config = ConfigurationManager.GetSection("git") as Hashtable;
-            try
-            {
-                gitExe = (string)config["gitExe"] ?? gitExe;
-                remote = (string)config["remote"] ?? remote;
-                this.branch = branch ?? (string)config["branch"] ?? this.branch;
-            }
-            catch (KeyNotFoundException Ex)
-            {
-                Console.WriteLine("Missing git config parameter in app.config");
-                throw Ex;
-            }
-        }
+			Hashtable config = ConfigurationManager.GetSection("git") as Hashtable;
+			try {
+				gitExe = (string)config["gitExe"] ?? gitExe;
+				remote = (string)config["remote"] ?? remote;
+				this.branch = branch ?? (string)config["branch"] ?? this.branch;
+			} catch (KeyNotFoundException Ex) {
+				Console.WriteLine("Missing git config parameter in app.config");
+				throw Ex;
+			}
+		}
 
-        ///<summary>
-        /// Runs arbitrary git commands
-        /// </summary>
-        /// <returns>Git command result as a string</returns>
-        public string ExecuteCommand(string command)
-        {
-            Process git = new Process();
-            ProcessStartInfo gitStartInfo = new ProcessStartInfo();
-            gitStartInfo.FileName = this.gitExe;
-            gitStartInfo.Arguments = command;
-            gitStartInfo.UseShellExecute = false;
-            gitStartInfo.RedirectStandardOutput = true;
-            gitStartInfo.WorkingDirectory = this.workingDir;
-            git.StartInfo = gitStartInfo;
-            git.Start();
-            string gitOutput = git.StandardOutput.ReadToEnd();
-            git.WaitForExit();
+		///<summary>
+		/// Runs arbitrary git commands
+		/// </summary>
+		/// <returns>Git command result as a string</returns>
+		public string ExecuteCommand(string command) {
+			Process git = new Process();
+			ProcessStartInfo gitStartInfo = new ProcessStartInfo();
+			gitStartInfo.FileName = gitExe;
+			gitStartInfo.Arguments = command;
+			gitStartInfo.UseShellExecute = false;
+			gitStartInfo.RedirectStandardOutput = true;
+			gitStartInfo.RedirectStandardError = true;
+			gitStartInfo.WorkingDirectory = workingDir;
+			git.StartInfo = gitStartInfo;
+			git.Start();
+			string gitOutput = git.StandardOutput.ReadToEnd();
+			git.WaitForExit();
 
-            return gitOutput;
-        }
+			return gitOutput;
+		}
 
-        /// <summary>
-        /// Update the local repository with the lastest upstream changes
-        /// </summary>
-        public void UpdateLocalRepo()
-        {
-            RemoteUpdate();
-            CheckoutBranch();
-        }
+		/// <summary>
+		/// Update the local repository with the lastest upstream changes
+		/// </summary>
+		public void UpdateLocalRepo() {
+			RemoteUpdate();
+			CheckoutBranch();
+		}
 
-        /// <summary>
-        /// Make sure the local repository is on the correct branch. Assumes master will be used.
-        /// </summary>
-        private void CheckoutBranch()
-        {
-            // Checkout the desired branch
-            Console.WriteLine(this.ExecuteCommand("checkout " + branch));
-            // Update everything to match the remote branch exactly including removing untracked files if there are any
-            Console.WriteLine(this.ExecuteCommand("reset --hard " + remote + "/" + branch));
-        }
+		/// <summary>
+		/// Make sure the local repository is on the correct branch. Assumes master will be used.
+		/// </summary>
+		private void CheckoutBranch() {
+			// Checkout the desired branch
+			Console.WriteLine(ExecuteCommand("checkout " + branch));
+			// Update everything to match the remote branch exactly including removing untracked files if there are any
+			Console.WriteLine(ExecuteCommand("reset --hard " + remote + "/" + branch));
+		}
 
-        /// <summary>
-        /// Get the latest updates from the remote
-        /// </summary>
-        private void RemoteUpdate()
-        {
-            Console.WriteLine(this.ExecuteCommand("remote update " + remote));
-        }
+		/// <summary>
+		/// Get the latest updates from the remote
+		/// </summary>
+		private void RemoteUpdate() {
+			Console.WriteLine(ExecuteCommand("remote update " + remote));
+		}
 
-        /// <summary>
-        /// Pull the remote updates into the local repository (only from origin not upstream)
-        /// </summary>
-        private void PullChanges()
-        {
-            Console.WriteLine(this.ExecuteCommand("pull " + remote));
-        }
+		/// <summary>
+		/// Pull the remote updates into the local repository (only from origin not upstream)
+		/// </summary>
+		private void PullChanges() {
+			Console.WriteLine(ExecuteCommand("pull " + remote));
+		}
 
-        /// <summary>
-        /// Fetch the hash for the HEAD of the current branch
-        /// </summary>
-        /// <returns>Git hash as a string</returns>
-        public string HeadVersion()
-        {
-            return this.ExecuteCommand("rev-parse HEAD").Trim();
-        }
+		/// <summary>
+		/// Fetch the hash for the HEAD of the current branch
+		/// </summary>
+		/// <returns>Git hash as a string</returns>
+		public string HeadVersion() {
+			return ExecuteCommand("rev-parse HEAD").Trim();
+		}
 
-        /// <summary>
-        /// Find files in the local directory that need to be migrated to the target database
-        /// </summary>
-        /// <param name="databaseVersionHash">The most recent git hash stored in the target database to be updated</param>
-        /// <param name="repoVersionHash">The hash in the local repo that we want to compare the database hash to (typically HEAD)</param>
-        /// <returns>List of files prefixed with relative paths to the local repo</returns>
-        public string[] GetScripts(string databaseVersionHash, string repoVersionHash)
-        {
-            // Get all files that have been updated since the database was last updated
-            // (M)odified, (A)dded, (C)opied, (R)enamed
-            // (Renamed doesn't seem to do anything in testing but is here for consistency and claritys since -CM seem to catch renamed files)
-            var gitDiff = string.Format("diff -O diff-order-migrations-first --name-only --diff-filter=MACR {0}..{1}", databaseVersionHash, repoVersionHash);
-            return this.ExecuteCommand(gitDiff).Split('\n');
-        }
-    }
+		/// <summary>
+		/// Find files in the local directory that need to be migrated to the target database
+		/// </summary>
+		/// <param name="databaseVersionHash">The most recent git hash stored in the target database to be updated</param>
+		/// <param name="repoVersionHash">The hash in the local repo that we want to compare the database hash to (typically HEAD)</param>
+		/// <returns>List of files prefixed with relative paths to the local repo</returns>
+		public string[] GetScripts(string databaseVersionHash, string repoVersionHash) {
+			// Get all files that have been updated since the database was last updated
+			// (M)odified, (A)dded, (C)opied, (R)enamed
+			// (Renamed doesn't seem to do anything in testing but is here for consistency and claritys since -CM seem to catch renamed files)
+			return ExecuteCommand(
+				String.Format(
+					"diff -O diff-order-migrations-first --name-only --diff-filter=MACR {0}..{1}",
+					databaseVersionHash,
+					repoVersionHash
+				)
+			).Split('\n');
+		}
+	}
 }
